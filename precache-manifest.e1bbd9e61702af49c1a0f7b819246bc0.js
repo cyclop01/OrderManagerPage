c1b5f84715353a8314157db35a3823f7e6619791
Lucas Oliveira <lucas_c10f@hotmail.com>
2021-04-26 19:22:19 -0300

    First Commit

---
+++ precache-manifest.e1bbd9e61702af49c1a0f7b819246bc0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2b4c390068c0b56451b0e11ae4bbc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17b5733b99c5fc30e9aa",
+    "url": "/static/css/2.2f5a5a9c.chunk.css"
+  },
+  {
+    "revision": "ea0e2c505731a5ca4a38",
+    "url": "/static/css/main.ce36aee0.chunk.css"
+  },
+  {
+    "revision": "17b5733b99c5fc30e9aa",
+    "url": "/static/js/2.780c31dd.chunk.js"
+  },
+  {
+    "revision": "cfd5c38401db2fcd88e547ed3cf903cf",
+    "url": "/static/js/2.780c31dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86726f822a27566e6a32",
+    "url": "/static/js/3.3201406e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/3.3201406e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47501e620d3a7f59f340",
+    "url": "/static/js/4.fcde6437.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.fcde6437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c40e8b84fdb3205757",
+    "url": "/static/js/5.bc320d59.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.bc320d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0e2c505731a5ca4a38",
+    "url": "/static/js/main.177eb540.chunk.js"
+  },
+  {
+    "revision": "67a6d845723258c14dd0",
+    "url": "/static/js/runtime-main.1a7afa48.js"
+  },
+  {
+    "revision": "bab6f1ee9617b3f4375b4b4ccb818b01",
+    "url": "/static/media/SalesforceSans-Bold.bab6f1ee.ttf"
+  },
+  {
+    "revision": "76d3c8425c3ee7c56dd3dad04b9016ca",
+    "url": "/static/media/SalesforceSans-Light.76d3c842.ttf"
+  },
+  {
+    "revision": "6c9ddaa8a8cfa8df9d612612753d00b2",
+    "url": "/static/media/SalesforceSans-Regular.6c9ddaa8.ttf"
+  },
+  {
+    "revision": "b4b52fea9d4b0e87eb7a302197295cad",
+    "url": "/static/media/SalesforceSans-Thin.b4b52fea.ttf"
+  },
+  {
+    "revision": "4916d5e00b29f660c1a99654c7600626",
+    "url": "/static/media/background_public_layout.4916d5e0.jpg"
+  }
+]);
